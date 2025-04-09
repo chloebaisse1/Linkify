1da433505b3a752d5cfca54eeed9258061da62e8
chloebaisse1 <baissechloe@gmail.com>
2025-04-09 16:37:50 +0400

    signup & sign in avec clerk

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="flex h-screen justify-center items-center">
+      {" "}
+      <SignIn />
+    </div>
+  );
 }
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,5 +1,10 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="flex h-screen justify-center items-center">
+      {" "}
+      <SignUp />
+    </div>
+  );
 }
