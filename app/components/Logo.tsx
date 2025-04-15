701735741eba2c2c52dc90622e87ea3a0b41e5c1
chloebaisse1 <baissechloe@gmail.com>
2025-04-15 15:48:33 +0400

    affichage des differents thèmes

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,8 +1,15 @@
 import { AudioLines } from "lucide-react";
+import { FC } from "react";
+interface LogoProps {
+  theme?: string;
+}
 
-const Logo = () => {
+const Logo: FC<LogoProps> = ({ theme }) => {
   return (
-    <div className="flex items-center bg-transparent" data-theme="corporate">
+    <div
+      className="flex items-center bg-transparent"
+      data-theme={`${theme}` || "corporate"}
+    >
       <div>
         <AudioLines className="w-6 h-6 text-accent mr-2" />
       </div>
